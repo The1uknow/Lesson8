66448ba0a357dd91a235ab643d73fd2ba5359566
мухсин <mukhsinus@MacBook-Pro-Sardor.local>
2025-05-16 06:29:23 +0500

    14.05

---
+++ hw.py
@@ -0,0 +1,10 @@
+i = lambda q, w: q + w
+q = int(input("Enter a number: "))
+w = int(input("Enter one more number: "))
+print(i(q, w))
+
+
+# import requests
+# link = "https://classroom.google.com/c/NzczMjc1MTM2NDM3/a/NzY1NTg3MjM4ODY1/details"
+# response = requests.get(link).text
+# print(response)
---
+++ main.py
@@ -0,0 +1,10 @@
+#max = lambda a, b: (a if a > b else b)
+#print(max(3, 8))
+
+# p = lambda a: a*4
+# print(p(2))
+
+import requests
+
+i = 'https://icanhazip.com/'
+print(requests.get(i).text)
